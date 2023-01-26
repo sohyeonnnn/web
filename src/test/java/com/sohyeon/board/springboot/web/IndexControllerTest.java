7bd21c58b31c5e1dbaa448798b1df9478e1c97fc
sohyeonnnn <limsohyeonn.18@gmail.com>
2023-01-26 14:40:13 +0900

    google oauth 진행중

---
+++ src/test/java/com/sohyeon/board/springboot/web/IndexControllerTest.java
@@ -0,0 +1,29 @@
+package com.sohyeon.board.springboot.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+public class IndexControllerTest {
+    @Autowired
+    private TestRestTemplate restTemplate; //jpa관련 test할때
+
+    @Test
+    public void load_mainPage(){
+        //when
+        String body = this.restTemplate.getForObject("/", String.class);
+
+        //then
+        assertThat(body).contains("스프링 부트로 시작하는 웹 서비스");
+    }
+
+
+}
