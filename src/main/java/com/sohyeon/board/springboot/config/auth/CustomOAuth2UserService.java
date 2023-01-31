8b451b29ec44b4768515e7d83977e39c755934f3
sohyeonnnn <limsohyeonn.18@gmail.com>
2023-01-31 17:04:45 +0900

    google oauth 진행중

--- src/main/java/com/sohyeon/board/springboot/config/auth/CustomOAuth2UserService.java
+++ src/main/java/com/sohyeon/board/springboot/config/auth/CustomOAuth2UserService.java
@@ -28,15 +28,20 @@ public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequ
     OAuth2User oAuth2User = delegate.loadUser(userRequest);
 
     String registrationId = userRequest.getClientRegistration().getRegistrationId();
-    String userNameAttributeName = userRequest.getClientRegistration().getProviderDetails().getUserInfoEndpoint().getUserNameAttributeName();
+    String userNameAttributeName = userRequest.getClientRegistration().
+            getProviderDetails().getUserInfoEndpoint().getUserNameAttributeName();
 
-    OAuthAttributes attributes = OAuthAttributes.of(registrationId,userNameAttributeName,oAuth2User.getAttributes());
+    OAuthAttributes attributes = OAuthAttributes.
+            of(registrationId,userNameAttributeName,oAuth2User.getAttributes());
 
     User user = saveOrUpdate(attributes);
 
     httpSession.setAttribute("user", new SessionUser(user));
 
-    return new DefaultOAuth2User(Collections.singleton(new SimpleGrantedAuthority(user.getRoleKey(),attributes.getAttributes(),attributes.getNameAttributeKey());
+    return new DefaultOAuth2User(Collections.singleton(
+            new SimpleGrantedAuthority(user.getRoleKey())),
+            attributes.getAttributes(),
+            attributes.getNameAttributeKey());
     }
 
     private User saveOrUpdate(OAuthAttributes attributes){
--- src/main/java/com/sohyeon/board/springboot/web/IndexController.java
+++ src/main/java/com/sohyeon/board/springboot/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.sohyeon.board.springboot.web;
 
+import com.sohyeon.board.springboot.config.auth.LoginUser;
+import com.sohyeon.board.springboot.config.auth.dto.SessionUser;
 import com.sohyeon.board.springboot.service.posts.PostsService;
 import com.sohyeon.board.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,14 +10,22 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
     private final PostsService postsService;
+    // private final HttpSession httpsession;
 
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user){
         model.addAttribute("posts", postsService.findAllDesc());
+        // SessionUser user = (SessionUser) httpsession.getAttribute("user");
+
+        if(user!= null){
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
